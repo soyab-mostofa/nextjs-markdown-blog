2644670bf709fe81602ed348bee8af534d8ea6cc
soyabatwork <soyabmostofa@gmail.com>
2022-05-15 16:08:33 +0600

    first commit

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { marked } from "marked";
+
 import Link from "next/link";
 
 export default function PostPage({
